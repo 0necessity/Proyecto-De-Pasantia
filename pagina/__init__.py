4c57d056acb4a80b1ef093c45a0201d40ac986fd
lol kek <124595679+0necessity@users.noreply.github.com>
2023-04-17 11:57:46 -0400

    Previously I made another one that broke, so I remade it

---
+++ pagina/__init__.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = "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"
+    app.config['UPLOAD_FOLDER'] = r"C:\Users\pbedg\Desktop\Nueva carpeta (7)\yes\pagina\maybe"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    db.init_app(app)
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
+
+    from .models import User
+    create_database(app)
+    return app
+
+
+def create_database(app):
+    if not path.exists("pagina/" + DB_NAME):
+        with app.app_context():
+            db.create_all()
---
+++ pagina/auth.py
@@ -0,0 +1,268 @@
+import base64
+import datetime
+import email.charset
+import os
+import pathlib
+import re
+from flask import Blueprint, render_template, flash, url_for
+from flask import session, redirect, request
+from google_auth_oauthlib.flow import Flow
+from sqlalchemy import *
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+import requests
+from flask import session as sa
+from flask import Flask, session, abort, redirect, request
+from google.oauth2 import id_token
+from google_auth_oauthlib.flow import Flow
+from pip._vendor import cachecontrol
+import google.auth.transport.requests
+import random
+import string
+
+
+
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1" # to allow Http traffic for local dev
+
+GOOGLE_CLIENT_ID = "767012225869-o403codh716ib53pnug7pdhpmnqs7mid.apps.googleusercontent.com"
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+
+
+auth = Blueprint("auth", __name__)
+Base = declarative_base()
+
+
+class SignUp(Base):
+    try:
+        __tablename__ = "sign-up"
+        name = Column("name", String, unique=True)
+        emails = Column("emails", String, unique=True)
+        role = Column("role", String)
+        lastname = Column("lastname", String)
+        password = Column("password", String)
+        photo = Column("photo", String)
+        id = Column(Integer, primary_key=True, autoincrement=True)
+    except:
+        pass
+
+    def __init__(self, name,emails,role, lastname, password, photo):
+        self.name = name
+        self.emails = emails
+        self.role = role
+        self.lastname = lastname
+        self.password = password
+        self.photo = photo
+
+    def __repr__(self):
+        return f"{self.name} {self.emails} {self.role} {self.lastname} {self.password} {self.photo} {self.id}"
+
+
+engine = create_engine("sqlite:///mydb.db", echo=True)
+Base.metadata.create_all(bind=engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="http://127.0.0.1:5000/callback"
+)
+
+globo = {}
+def login_is_required(function):
+    def wrapper(*args, **kwargs):
+        if "google_id" not in sa:
+            return abort(401)  # Authorization required
+        else:
+            return function()
+
+    return wrapper
+
+
+@auth.route("/logan")
+def logan():
+    authorization_url, state = flow.authorization_url()
+    sa["state"] = state
+    return redirect(authorization_url)
+
+
+@auth.route("/callback")
+def callback():
+    flow.fetch_token(authorization_response=request.url)
+
+    if not sa["state"] == request.args["state"]:
+        abort(500)  # State does not match!
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+    global globo
+    globo = id_info
+
+    # trabaja con esto para que no repitas vaina
+    # r = session.query(SignUp).filter(SignUp.name == fname).all()
+    # s = session.query(SignUp).filter(SignUp.emails == enmail).all()
+
+    # huh = SignUp(
+    # session.add(huh)
+    # session.commit()
+    # print(session.query(SignUp).all())
+
+
+    return redirect(url_for('auth.continues'))
+
+
+
+@auth.route("/lagout")
+def lagout():
+    sa.clear()
+    return redirect("/")
+
+
+
+@auth.route("/protected_area")
+@login_is_required
+def protected_area():
+    return f"Hello {sa['name']}! <br/> <a href='/logout'><button>Logout</button></a>"
+
+
+@auth.route("/continuing", methods=['POST', "GET"])
+def continues():
+    if request.method == "POST":
+        print(globo)
+        chars = string.ascii_letters + string.digits + string.punctuation
+        enmail = globo["email"]
+        fname = globo["given_name"]
+        lname = globo["family_name"]
+        password1 = ''.join(random.choice(chars) for i in range(20))
+        phato = requests.get(globo["picture"])
+
+
+        # ASEGURAE DE QUE CODIFIQUE LA VAINA BIEN
+        photo = base64.b64encode(phato.content).decode('utf-8')
+
+        role = request.form.get("role")
+
+        huh = SignUp(fname, enmail, role, lname, password1, photo)
+        session.add(huh)
+        session.commit()
+        print(session.query(SignUp).all())
+        return redirect(url_for('auth.login'))
+
+    return render_template("tranquilo.html")
+
+
+@auth.route("/login", methods=['POST', "GET"])
+def login():
+    if request.method == "POST":
+        lo_password = str(request.form.get("password"))
+        lo_email = str(request.form.get("email")).lower()
+        ema =  session.query(SignUp).filter_by(emails=lo_email).all()
+        passo = session.query(SignUp).filter_by(password=lo_password).all()
+
+        print(len(ema))
+        print(len(passo))
+        # print(session.query(SignUp).all())
+        # print(lo_password)
+        if len(lo_email) < 1:
+            flash("Please enter an email", category="error")
+        elif len(lo_password) < 1:
+            flash("Please enter a password", category="error")
+        else:
+            if len(passo) < 1:
+                flash("Wrong password", category="error")
+            if len(ema) < 1:
+                flash("Wrong email", category="error")
+            if len(ema) == 1 and len(passo) == 1:
+                return redirect(url_for('views.home'))
+
+    return render_template("login.html", coco=23)
+
+
+@auth.route("/logout")
+def logout():
+    return ">:D"
+
+
+@auth.route("/sign_up", methods=['POST', "GET"])
+def sign_up():
+    yyears = []
+    for year in range(1900, 2024):
+        yyears.append(year)
+    years = yyears[::-1]
+
+    photo = b""
+    encoded_image = base64.b64encode(photo).decode('utf-8')
+    if request.method == "POST":
+        enmail = str(request.form.get("email")).lower()
+        fname = str(request.form.get("firstName"))
+        lname = str(request.form.get("lastName"))
+        password1 = str(request.form.get("password1"))
+        password2 = str(request.form.get("password2"))
+        photo = request.files["image"].read()
+        role = request.form.get("role")
+
+        encoded_image = base64.b64encode(photo).decode('utf-8')
+        pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        match = re.match(pattern, str(request.form.get("email")))
+        # ///////////////////////////
+        day = int(request.form['day'])
+        month = int(request.form['month'])
+        year = int(request.form['year'])
+        dob = datetime.date(year, month, day)
+        today = datetime.date.today()
+        age = today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
+        # ///////////////////////////
+        print(session.query(SignUp).all())
+        print("huh?")
+
+
+        r = session.query(SignUp).filter(SignUp.name == fname).all()
+        s = session.query(SignUp).filter(SignUp.emails == enmail).all()
+
+        print(password1)
+        if len(r) > 0:
+            flash("That name is already in use, please select a new one", category="error")
+        elif len(s) > 0:
+            flash("That email is already in use, please select a new one", category="error")
+        elif age < 18:
+            flash("You must be above 18 to enter", category="error")
+        elif password1 != password2:
+            flash("Your passwords need to be the same", category="error")
+        elif len(password1) < 8:
+            flash("Your password need to be larger", category="error")
+        elif len(fname) < 3:
+            flash("Your name need to be larger", category="error")
+        elif len(lname) < 3:
+            flash("Your last name need to be larger", category="error")
+        elif match is None:
+            flash("Please enter an valid email direction", category="error")
+        elif len(photo) < 0:  # CHANGE THIS TO 5 LATER!!!!!!!!!!!!!!!!
+            flash("Please enter an profile picture", category="error")
+        elif len(photo) > 5000000:
+            flash("The size of your profile picture is too big. Pleas select an smaller one", category="error")
+        # DE-COMMENT THIS LATER
+        # elif photo[:4] != b'\xff\xd8\xff\xe0' or photo[:4] == b'\xff\xd8\xff\xe1' or photo[:4] == b'\x89PNG':
+        #     flash("This type of files is not supported, please make sure to upload a PNG or JPEG file", category="error")
+        else:
+            print(password1)
+            huh = SignUp(fname, enmail, role, lname, password1, photo)
+            session.add(huh)
+            session.commit()
+            print(session.query(SignUp).all())
+            return redirect(url_for('auth.login'))
+
+        print(len(photo))
+        print(fname)
+        print(email)
+    return render_template("sign-up.html", image_data=encoded_image, y=years)
+
