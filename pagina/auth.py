0e7791b355a20918b53ef59520fe2135403e7099
lol kek <124595679+0necessity@users.noreply.github.com>
2023-04-18 12:03:22 -0400

    I've got to work on a couple of query's

--- pagina/auth.py
+++ pagina/auth.py
@@ -13,7 +13,7 @@ from sqlalchemy.orm import sessionmaker
 
 import requests
 from flask import session as sa
-from flask import Flask, session, abort, redirect, request
+from flask import Flask, session, abort, redirect, request, make_response
 from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow
 from pip._vendor import cachecontrol
@@ -29,6 +29,10 @@ client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret
 auth = Blueprint("auth", __name__)
 Base = declarative_base()
 
+# KNOW PROBLEM THAT I MAY OR MAY NOT FIX IN THE FUTURE:
+# THE COOKIES ARE SET TO IDs OF THE ENTRYS IN THE DATABASE THAT
+# ARE ORGANIZED WITH AUTO-INCREMENT MAKING IT SO ANYONE CAN JUST INJECT THEIR COOKIE AN ACCESS THE PROFILE OF SOMEONE
+
 
 class SignUp(Base):
     try:
@@ -39,20 +43,21 @@ class SignUp(Base):
         lastname = Column("lastname", String)
         password = Column("password", String)
         photo = Column("photo", String)
+        cookieid = Column("cookieid", String)
         id = Column(Integer, primary_key=True, autoincrement=True)
     except:
         pass
 
-    def __init__(self, name, emails, role, lastname, password, photo):
+    def __init__(self, name, emails, role, lastname, password, photo, cookieid):
         self.name = name
         self.emails = emails
         self.role = role
         self.lastname = lastname
         self.password = password
         self.photo = photo
-
+        self.cookieid = cookieid
     def __repr__(self):
-        return f"{self.name} {self.emails} {self.role} {self.lastname} {self.password} {self.photo} {self.id}"
+        return f"{self.name} {self.emails} {self.role} {self.lastname} {self.password} {self.photo} {self.cookieid} {self.id}"
 
 
 engine = create_engine("sqlite:///mydb.db", echo=True)
@@ -68,6 +73,7 @@ flow = Flow.from_client_secrets_file(
 )
 
 globo = {}
+
 
 
 # def login_is_required(function):
@@ -92,7 +98,7 @@ def callback():
     flow.fetch_token(authorization_response=request.url)
 
     if not sa["state"] == request.args["state"]:
-        abort(500)  # State does not match!
+        abort(500)
 
     credentials = flow.credentials
     request_session = requests.session()
@@ -107,8 +113,24 @@ def callback():
 
     s = session.query(SignUp).filter(SignUp.emails == id_info["email"]).all()
     if len(s) > 0:
-        flash("That email is already in use, please select a new one", category="error")
-        return redirect(url_for('auth.sign_up'))
+        # HERES WHERE YOU ARE GONNA GET THE COOKIE
+        res = make_response(redirect(url_for('views.home')))
+        cookie = request.cookies
+        stat = cookie.get("user")
+        print(s)
+        print("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+        print(len(s))
+        try:
+            print(s[7])
+        except:
+            pass
+
+        print(stat)
+        if stat is None:
+            print(s)
+            # need to make a query here:
+            res.set_cookie("user", id_info["given_name"], 600)
+        return res
 
     global globo
     globo = id_info
@@ -116,10 +138,10 @@ def callback():
     return redirect(url_for('auth.continues'))
 
 
-@auth.route("/lagout")
-def lagout():
-    sa.clear()
-    return redirect("/")
+# @auth.route("/lagout")
+# def lagout():
+#     sa.clear()
+#     return redirect("/")
 
 
 # @auth.route("/protected_area")
@@ -130,8 +152,6 @@ def lagout():
 
 @auth.route("/continuing", methods=['POST', "GET"])
 def continues():
-    # NOW THERES MORE THING YOU GOTTA WORK ON WITH THE PICTURES........
-    # BUT FIRST!! MAKE SURE THERE IS NOTHING ELSE OAUTH ISN'T MISSING
     if "family_name" in globo:
         lname_is_missing = False
     else:
@@ -145,26 +165,33 @@ def continues():
         else:
             lname = request.form.get("lname")
 
+        if "picture" in globo:
+            print(globo["picture"])
+            print(requests.get(globo["picture"]))
+            photo = requests.get(globo["picture"]).content
+        else:
+            image_path = os.path.join(os.getcwd(), 'pagina', 'static', "default.jpg")
+            with open(image_path, 'rb') as image_file:
+                photo = image_file.read()
+
         chars = string.ascii_letters + string.digits + string.punctuation
         enmail = globo["email"]
         fname = globo["given_name"]
         password1 = ''.join(random.choice(chars) for i in range(20))
-        phato = requests.get(globo["picture"])
-
-        # ASEGURAE DE QUE CODIFIQUE LA VAINA BIEN
-        photo = base64.b64encode(phato.content).decode('utf-8')
-
         role = request.form.get("role")
+        chars = string.ascii_letters + string.digits + string.punctuation
+        cookieID = ''.join(random.choice(chars) for i in range(30))
 
-        huh = SignUp(fname, enmail, role, lname, password1, photo)
+        huh = SignUp(fname, enmail, role, lname, password1, photo, cookieID)
         session.add(huh)
         session.commit()
         print(session.query(SignUp).all())
 
-        global globo
-        globo = {}
-
-        return redirect(url_for('auth.login'))
+        res = make_response(redirect(url_for('auth.login')))
+        # cHANGE IT so it sets id instead of name
+        res.set_cookie("user", cookieID, 600)
+        # HERES WHERE YOU ARE GONNA SET THE COOKIE
+        return res
     return render_template("tranquilo.html", lname_status=lname_is_missing)
 
 
@@ -175,11 +202,8 @@ def login():
         lo_email = str(request.form.get("email")).lower()
         ema = session.query(SignUp).filter_by(emails=lo_email).all()
         passo = session.query(SignUp).filter_by(password=lo_password).all()
+        # THERES A PROBLEM, THIS ONLY CHECK IF YOU PUT AN EMAIL THAT EXIST AND AN PASSWORD THAT EXIST
 
-        print(len(ema))
-        print(len(passo))
-        # print(session.query(SignUp).all())
-        # print(lo_password)
         if len(lo_email) < 1:
             flash("Please enter an email", category="error")
         elif len(lo_password) < 1:
@@ -190,7 +214,17 @@ def login():
             if len(ema) < 1:
                 flash("Wrong email", category="error")
             if len(ema) == 1 and len(passo) == 1:
-                return redirect(url_for('views.home'))
+                # HERES WHERE YOU ARE GONNA GET THE COOKIE
+                res = make_response(redirect(url_for('views.home')))
+                cookie = request.cookies
+                stat = cookie.get("user")
+                print("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+                print(stat)
+                if stat is None:
+                    #this dosent work, really work with IDs
+                    #res.set_cookie("user", id_info["given_name"], 600)
+                    pass
+                return res
 
     return render_template("login.html", coco=23)
 
@@ -217,7 +251,7 @@ def sign_up():
         password2 = str(request.form.get("password2"))
         photo = request.files["image"].read()
         role = request.form.get("role")
-
+        # This is neede it so you can display the saved images in your website
         encoded_image = base64.b64encode(photo).decode('utf-8')
         pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
         match = re.match(pattern, str(request.form.get("email")))
@@ -252,20 +286,31 @@ def sign_up():
             flash("Your last name need to be larger", category="error")
         elif match is None:
             flash("Please enter an valid email direction", category="error")
-        elif len(photo) < 0:  # CHANGE THIS TO 5 LATER!!!!!!!!!!!!!!!!
-            flash("Please enter an profile picture", category="error")
         elif len(photo) > 5000000:
-            flash("The size of your profile picture is too big. Pleas select an smaller one", category="error")
-        # DE-COMMENT THIS LATER
-        # elif photo[:4] != b'\xff\xd8\xff\xe0' or photo[:4] == b'\xff\xd8\xff\xe1' or photo[:4] == b'\x89PNG':
+            flash("The size of your profile picture is too big. Please select an smaller one", category="error")
+        # DE-COMMENT THIS LATER (MAKE TEST ON THIS TOO TO MAKE SURE IT WORKS WELL)
+        # elif not photo[:4] == b'\xff\xd8\xff\xe0' or photo[:4] == b'\xff\xd8\xff\xe1' or photo[:4] == b'\x89PNG' or photo == None:
         #     flash("This type of files is not supported, please make sure to upload a PNG or JPEG file", category="error")
         else:
-            print(password1)
-            huh = SignUp(fname, enmail, role, lname, password1, photo)
+            if len(photo) < 5:
+                image_path = os.path.join(os.getcwd(), 'pagina', 'static', "default.jpg")
+                with open(image_path, 'rb') as image_file:
+                    photo = image_file.read()
+
+
+            #YOU PROB SHOULD MAKE THIS INTO A FUNCTION
+            chars = string.ascii_letters + string.digits + string.punctuation
+            cookieID = ''.join(random.choice(chars) for i in range(30))
+
+            huh = SignUp(fname, enmail, role, lname, password1, photo, cookieID)
             session.add(huh)
             session.commit()
             print(session.query(SignUp).all())
-            return redirect(url_for('auth.login'))
+            # HERES WHERE YOU ARE GONNA SET THE COOKIE
+            res = make_response(redirect(url_for('auth.login')))
+            # cHANGE IT so it sets id instead of name
+            res.set_cookie("user", cookieID, 600)
+            return res
 
         print(len(photo))
         print(fname)
